0f7c902ef8bc375367f4d79b1413eadb92b64391
Mamun8854 <mamunabdullah8854@gmail.com>
2022-12-28 17:34:45 +0600

    updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // mongodb uri and client
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nuhoilk.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -24,6 +24,10 @@ const run = async () => {
   try {
     // post collection
     const postsCollection = client.db("endgame1").collection("posts");
+
+    // comment collection
+
+    const commentCollection = client.db("endgame1").collection("comment");
 
     // post data POST
     app.post("/post", async (req, res) => {
@@ -44,6 +48,38 @@ const run = async () => {
       const query = { _id: ObjectId(id) };
       const result = await postsCollection.findOne(query);
       res.send(result);
+    });
+
+    // comment section
+
+    app.post("/comment", async (req, res) => {
+      const comment = req.body;
+      const result = await commentCollection.insertOne(comment);
+      res.send(result);
+    });
+
+    // get comment data from db
+    app.get("/allComments", async (req, res) => {
+      const query = {};
+      const result = await commentCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/comments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { postId: id };
+      const postId = commentCollection.find(query).sort({ date: -1 });
+      const result = await postId.toArray();
+      res.send(result);
+    });
+
+    // get 3 post from posts collection by sorting
+    app.get("/homePost", async (req, res) => {
+      const size = parseInt(req.query.size);
+      const query = {};
+      const cursor = postsCollection.find(query);
+      const posts = await cursor.limit(size).sort({ date: -1 }).toArray();
+      res.send(posts);
     });
   } catch {}
 };
